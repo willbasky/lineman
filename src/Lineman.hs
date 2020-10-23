98941100fc9ae87da7b74f790e18a35cac7d2978
Vladislav Sabanov <vladislav.sabanov@gmail.com>
2020-10-23 19:19:23 +0500

    Fix travis (#3)
    * Fix travis
    
    * Relax travis config
    
    * Setup travis
    
    * Bump up cabal version
    
    * Bump up path-io
    
    Co-authored-by: Vladislav Sabanov <vladislav.sabanov@juspay.in>

--- src/Lineman.hs
+++ src/Lineman.hs
@@ -1,12 +1,10 @@
 {-# LANGUAGE DerivingStrategies #-}
-module Lineman
-    (
-    ) where
+module Lineman where
 
 import Relude
-import Relude.String
+-- import Relude.String
 
-import Path.IO hiding (findFiles, findFilesWith)
+import Path.IO
 import Path.Posix
 
 
@@ -16,7 +14,7 @@ dirForClean = do
   stack <- parseRelFile "stack.yaml"
   print stack
   print dir
-  (dirs, files) <- listDirRecur dir
+  (dirs, _) <- listDirRecur dir
   findFiles dirs stack
 
 
@@ -28,62 +26,3 @@ data Config = Config
   , deepLevel :: Word8
   }
   deriving stock (Show, Eq)
-
-
-
-
-
-
-
-
-
-
-
-
--- Used here until origin repo be not fixed
-
-findFiles ::
-  MonadIO m =>
-  -- | Set of directories to search in
-  [Path b Dir] ->
-  -- | Filename of interest
-  Path Rel File ->
-  -- | Absolute paths to all found files
-  m [Path Abs File]
-findFiles = findFilesWith (const (return True))
-
--- | Search through the given set of directories for the given file and with
--- the given property (usually permissions) and return a list of paths where
--- the given file exists and has the property.
-findFilesWith ::
-  MonadIO m =>
-  -- | How to test the files
-  (Path Abs File -> m Bool) ->
-  -- | Set of directories to search in
-  [Path b Dir] ->
-  -- | Filename of interest
-  Path Rel File ->
-  -- | Absolute paths to all found files
-  m [Path Abs File]
-findFilesWith _ [] _ = return []
-findFilesWith f (d : ds) file = do
-  bfile <- (</> file) <$> makeAbsolute d
-  exist <- doesFileExist bfile
-  b <- if exist then f bfile else return False
-  if b
-    then (bfile :) <$> findFilesWith f ds file
-    else findFilesWith f ds file
-
-
-
-
-
-
-
-
-
-
-
-
-
-
